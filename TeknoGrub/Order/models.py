b7661d166e1412d1891c9b1efe2980f22b2d8574
neenja <janeengabrielle.lim@cit.edu>
2025-09-27 06:13:33 -0700

    Changes in Order

--- TeknoGrub/Order/models.py
+++ TeknoGrub/Order/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from Cart.models import Cart
+#from User.models import Staff
 
 # Create your models here.
 
@@ -11,9 +12,9 @@ class Order(models.Model):
     note = models.TextField(blank=True, null=True)
     is_reorder = models.BooleanField(default=False)
 
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="orders")
+    user = models.ForeignKey("user.User", on_delete=models.CASCADE, related_name="orders")
     cart = models.ForeignKey(Cart, on_delete=models.SET_NULL, null=True, blank=True, related_name="orders")
-
+    staff = models.ForeignKey("user.Staff", on_delete=models.CASCADE)
 
 class OrderItem(models.Model):
     order_item = models.AutoField(primary_key=True)
@@ -24,7 +25,6 @@ class OrderItem(models.Model):
 
     order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="items")
     menu_item = models.ForeignKey("canteens.MenuItem", on_delete=models.CASCADE, related_name="order_items")
-
 
 class OrderItemAddOn(models.Model):
     oder_item_addon = models.AutoField(primary_key=True)
@@ -38,4 +38,4 @@ class Staff(models.Model):
     login_logs = models.TextField(blank=True, null=True)
     attendance_logs = models.TextField(blank=True, null=True)
 
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="processed_by")
+    #order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="processed_by")
