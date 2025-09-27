3e2848fba6d7469ba39604250448a2eeb1a2a65b
dAEcoder <ashleesean2015@gmail.com>
2025-09-27 21:17:09 +0800

    updated canteen mowdels

--- TeknoGrub/Canteen/models.py
+++ TeknoGrub/Canteen/models.py
@@ -1,22 +1,8 @@
 from django.db import models
 
-from TeknoGrub import User
-from TeknoGrub.Order.models import Order
-from TeknoGrub.User.models import User
-
-
 # Create your models here.
 class Canteen(models.Model):
     canteen_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
     contact_info = models.CharField(max_length=100)
-
-class Staff(models.Model):
-    staff_id = models.AutoField(primary_key=True)
-    log_in_logs = models.DateTimeField(auto_now_add=True)
-    attendance_logs = models.DateTimeField(auto_now_add=True)
-
-    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
-    canteen_id = models.ForeignKey(Canteen, on_delete=models.CASCADE)
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
