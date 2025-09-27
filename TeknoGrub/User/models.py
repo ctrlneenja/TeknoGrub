b06fd912793ddba4ff6d097cfc7fa1aad6f8338d
Hermoine <zhermoine@gmail.com>
2025-09-27 21:29:29 +0800

    Added User App

--- TeknoGrub/User/models.py
+++ TeknoGrub/User/models.py
@@ -1,1 +1,26 @@
 from django.db import models
+
+from TeknoGrub.Canteen.models import Canteen
+
+# Create your models here.
+class Role(models.Model):
+    role_id = models.AutoField(primary_key=True)
+    role_name = models.CharField(max_length=100)
+
+class Users(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    role_id = models.ForeignKey(Role, on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=200)
+    email = models.CharField(max_length=50)
+    phone = models.IntegerField()
+    password = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    school_id = models.CharField(max_length=20)
+
+class Staff(models.Model):
+    staff_id = models.AutoField(primary_key=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    canteen_id = models.ForeignKey(Canteen, on_delete=models.CASCADE, related_name='staff')
+    login_logs = models.DateTimeField(auto_now_add=True)
+    attendance_logs = models.DateTimeField()
+
