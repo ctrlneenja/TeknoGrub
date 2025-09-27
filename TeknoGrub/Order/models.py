1aba662c46d3a0fc0cafcc27bda54f91ecc684e7
neenja <janeengabrielle.lim@cit.edu>
2025-09-27 05:46:36 -0700

    Added new apps and changes with Order App

--- TeknoGrub/Order/models.py
+++ TeknoGrub/Order/models.py
@@ -1,31 +1,8 @@
 from django.db import models
+from Cart.models import Cart
+
 
 # Create your models here.
-
-class Cart(models.Model):
-    cart = models.AutoField(primary_key=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="carts")
-
-
-class CartItem(models.Model):
-    cart_item = models.AutoField(primary_key=True)
-    quantity = models.IntegerField()
-    note = models.TextField(blank=True, null=True)
-    added_at = models.DateTimeField(auto_now_add=True)
-
-    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name="items")
-    menu_item = models.ForeignKey("canteens.MenuItem", on_delete=models.CASCADE, related_name="cart_items")
-
-
-class CartItemAddOn(models.Model):
-    cart_item_addon = models.AutoField(primary_key=True)
-    cart_item = models.ForeignKey(CartItem, on_delete=models.CASCADE, related_name="addons")
-    addon = models.ForeignKey("canteens.AddOn", on_delete=models.CASCADE)
-    quantity = models.IntegerField(default=1)
-
 
 class Order(models.Model):
     order = models.AutoField(primary_key=True)
@@ -56,17 +33,6 @@ class OrderItemAddOn(models.Model):
     addon = models.ForeignKey("canteens.AddOn", on_delete=models.CASCADE)
     quantity = models.IntegerField()
     price_at_order = models.DecimalField(max_digits=10, decimal_places=2)
-
-
-class Payment(models.Model):
-    payment_id = models.AutoField(primary_key=True)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    transaction_ref = models.CharField(max_length=100)
-    payment_time = models.DateTimeField(auto_now_add=True)
-
-    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="payments")
-    payment_method = models.ForeignKey("users.UserPaymentMethod", on_delete=models.SET_NULL, null=True, blank=True)
-
 
 class Staff(models.Model):
     staff_id = models.AutoField(primary_key=True)
