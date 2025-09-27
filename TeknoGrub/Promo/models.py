0ddcb41c688d477333bcbc9b3f47f5eceb6b9918
neenja <janeengabrielle.lim@cit.edu>
2025-09-27 05:20:55 -0700

    Promo Model with primary key and naming changes

--- TeknoGrub/Promo/models.py
+++ TeknoGrub/Promo/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 # Create your models here.
 class Category (models.Model):
-    categoryName = models.CharField(max_length=100)
+    category_id = models.AutoField(primary_key=True)
+    category_name = models.CharField(max_length=100)
 
     def __str__(self):
-        return self.categoryName
+        return self.category_name
 
 class PromoName (models.Model):
+    promo_id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
     code = models.CharField(max_length=50, unique=True)
@@ -25,16 +27,19 @@ class PromoName (models.Model):
 
 
 class PromoItem (models.Model):
+    promo_item_id = models.AutoField(primary_key=True)
     promo = models.ForeignKey(PromoName, on_delete=models.CASCADE, related_name="items")
     menu_item = models.ForeignKey("canteens.MenuItem", on_delete=models.CASCADE, related_name="promos")
 
 
 class PromoCategory (models.Model):
+    promo_id = models.AutoField(primary_key=True)
     promo = models.ForeignKey(PromoName, on_delete=models.CASCADE, related_name="categories")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="promos")
 
 
 class PromoRedemption(models.Model):
+    promo_redemption_id = models.AutoField(primary_key=True)
     redeemed_at = models.DateTimeField(auto_now_add=True)
 
     user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name="redeemed_promos")
