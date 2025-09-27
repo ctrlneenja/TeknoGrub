6c03e1d3ad266f165f84cca03df0e2ea9a2e0d59
dAEcoder <ashleesean2015@gmail.com>
2025-09-27 20:21:25 +0800

    tan awa daw ako canteen mowdels.py

--- TeknoGrub/Canteen/models.py
+++ TeknoGrub/Canteen/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+class Canteen(models.Model):
+    canteen_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    contact_info = models.CharField(max_length=100)
+
+class MenuItem(models.Model):
+    menu_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image_url = models.URLField()
+    is_available = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    canteen = models.ForeignKey(Canteen, on_delete=models.CASCADE)
+
+class Inventory(models.Model):
+    inventory_id = models.AutoField(primary_key=True)
+    current_stock = models.IntegerField()
+    threshold_level = models.IntegerField()
+
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+
+class AddOn(models.Model):
+    add_on_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_available = models.BooleanField(default=True)
+
+class MenuItemAddOn(models.Model):
+    menu_item_id = models.AutoField(primary_key=True)
+    is_optional = models.BooleanField(null = True)
+
+    menu_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    add_on = models.ForeignKey(AddOn, on_delete=models.CASCADE)
+
+class MenuItemCategories(models.Model):
+    menu_item_category_id = models.AutoField(primary_key=True)
+
+    menu_id = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
--- TeknoGrub/Order/models.py
+++ TeknoGrub/Order/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class Cart(models.Model):
     cart = models.AutoField(primary_key=True)
     created_at = models.DateTimeField(auto_now_add=True)
