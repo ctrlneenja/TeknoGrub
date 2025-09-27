baf7b476f8da7cb3932f426a0af62415a1ab00cf
neenja <janeengabrielle.lim@cit.edu>
2025-09-27 07:05:21 -0700

    Update

--- TeknoGrub/Payment/models.py
+++ TeknoGrub/Payment/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from Order.models import Order
 
 # Create your models here.
+class UserPaymentMethod(models.Model):
+    CARD = "CARD"
+    GCASH = "GCASH"
+    PAYMAYA = "PAYMAYA"
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    payment_method_choices = [
+        (CARD, "Credit/Debit Card"),
+        (GCASH, "GCash"),
+        (PAYMAYA, "PayMaya"),
+    ]
+
+    method_type = models.CharField(
+        max_length=50,
+        choices= payment_method_choices,
+        default=CARD  # Optional: set a default choice
+    )
+    details = models.TextField()
+
 class Payment(models.Model):
     payment_id = models.AutoField(primary_key=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
