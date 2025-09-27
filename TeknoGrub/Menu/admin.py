39f6fb82267ca970d747de6d834e6bdfdc159706
dAEcoder <ashleesean2015@gmail.com>
2025-09-27 21:53:14 +0800

    added admin to the canteen menu and user admin chuchu

--- TeknoGrub/Menu/admin.py
+++ TeknoGrub/Menu/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(AddOn)
+admin.site.register(MenuItemAddOn)
+admin.site.register(MenuItemCategories)
--- TeknoGrub/User/admin.py
+++ TeknoGrub/User/admin.py
@@ -2,3 +2,6 @@ from django.contrib import admin
 from .models import Role, Users, Staff
 
 # Register your models here.
+admin.site.register(Role)
+admin.site.register(Users)
+admin.site.register(Staff)
