539b65b14d2e8411ad049682fa1e084008dd63ad
Hermoine <zhermoine@gmail.com>
2025-10-18 19:08:27 +0800

    Added view and urls in User app

--- TeknoGrub/Order/admin.py
+++ TeknoGrub/Order/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Order, OrderItem, OrderItemAddOn, Staff
+from .models import Order, OrderItem, Staff
 
 # Register your models here.
 admin.site.register(Order)
--- TeknoGrub/Promo/admin.py
+++ TeknoGrub/Promo/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Category, PromoName, PromoItem, PromoCategory, PromoRedemption
+from .models import PromoName, PromoRedemption
 
 # Register your models here.
-admin.site.register(Category)
+#admin.site.register(Category)
 admin.site.register(PromoName)
 # admin.site.register(PromoItem)
 # admin.site.register(PromoCategory)
